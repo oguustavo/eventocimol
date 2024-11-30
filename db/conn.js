06391b44e4f4909633b493ed5565934b8a07868c
Gustavo Soares <gustavosoaresok@gmail.com>
2024-11-30 17:34:05 -0300

    gh

--- db/conn.js
+++ db/conn.js
@@ -1,26 +1,28 @@
 const { Sequelize } = require('sequelize')
 require('dotenv').config()
 
-const sequelize = new Sequelize(
-    process.env.DB_NAME || 'sql10748608',
-    process.env.DB_USER || 'sql10748608',
-    process.env.DB_PASSWORD || '7Us75d96Qr',
-    {
-        host: process.env.DB_HOST || 'sql10.freesqldatabase.com',
-        dialect: 'mysql',
-        port: process.env.DB_PORT || 3306,
-        dialectModule: require('mysql2'),
-        logging: false,
-        pool: {
-            max: 5,
-            min: 0,
-            acquire: 30000,
-            idle: 10000
-        },
-        dialectOptions: {
-            connectTimeout: 60000
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: 'postgres',
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false
         }
+    },
+    logging: false,
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
     }
-)
+})
+
+try {
+    sequelize.authenticate()
+    console.log('Conexão estabelecida com sucesso.')
+} catch (error) {
+    console.error('Não foi possível conectar ao banco de dados:', error)
+}
 
 module.exports = sequelize
